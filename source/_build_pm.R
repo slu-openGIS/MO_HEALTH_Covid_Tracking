e7e905ff66b052dc61d8731b0c95fc1696e4e9bf
Christopher Prener <chris.prener@slu.edu>
2020-08-10 19:17:23 -0400

    build pm data for 2020-08-10

--- source/_build_pm.R
+++ source/_build_pm.R
@@ -17,7 +17,7 @@ source("source/functions/wrangle_zip.R")
 
 # workflow
 source("source/workflow/05_create_zips.R")
-# source("source/workflow/06_create_testing.R")
+source("source/workflow/06_create_testing.R")
 source("source/workflow/07_create_stl_hospital.R")
 source("source/workflow/08_create_kc_counties.R")
 
