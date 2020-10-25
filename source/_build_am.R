fb9397baabc0013c5bc43480e780f93006b4802d
Christopher Prener <chris.prener@slu.edu>
2020-10-25 11:24:12 -0400

    build in automatic ltc download

--- source/_build_am.R
+++ source/_build_am.R
@@ -15,6 +15,7 @@ library(tigris)
 
 ## other
 library(janitor)    # data wrangling
+library(rjson)      # parse json
 library(zoo)        # moving averages
 
 ## functions
@@ -28,15 +29,27 @@ date <- Sys.Date()-1
 
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
+# check ltc metadata for update
+update <- fromJSON(file = "https://data.cms.gov/api/views/metadata/v1/s2uc-8wxp")
+update <- as.Date(update$dataUpdatedAt)
+load("data/source/ltc/last_update.rda")
+
+# ==== # === # === # === # === # === # === # === # === # === # === # === # === #
+
 # execute workflow ####
+## daily workflow elements
 source("source/workflow/01_scrape_and_tidy.R")
 source("source/workflow/02_create_state_msa.R")
 source("source/workflow/03_add_rates.R")
 source("source/workflow/04_create_spatial.R")
 source("source/workflow/05_create_regions.R")
-# source("source/workflow/10_create_ltc.R")
+
+## weekly workflow elements
+if ((update == last_update$current_date) == FALSE){
+  source("source/workflow/10_create_ltc.R") 
+}
 
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
 # clean-up ####
-rm(date)
+rm(date, update, last_update)
