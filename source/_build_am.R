db1f881cfffe13c598b6f1b082811b1f25023d47
Christopher Prener <chris.prener@slu.edu>
2020-11-08 11:44:17 -0500

    build am data for 2020-11-08

--- source/_build_am.R
+++ source/_build_am.R
@@ -1,19 +1,43 @@
-# Construct AM Data Sets
+# Build AM Data ####
+
+#===# #===# #===# #===# #===# #===# #===# #===# #===# #===# #===# #===# #===# #===#
+
+# these data include:
+#   - the New York Times county data, which is used to build county, metro,
+#     regional, and state data sets for MO and adjacent areas
+#   - CMS nursing home data, but only if it has been updated
+
+# ==== # === # === # === # === # === # === # === # === # === # === # === # === #
+
+# evaluate last update to New York Times data ####
+
+## load function
+source("source/functions/get_last_update.R")
+
+## check last update
+q <- get_last_update()
+
+## evaluate last update
+if (q == FALSE){
+  stop("AM update aborted!")
+}
 
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
 # dependencies ####
-## tidyverse
+
+## packages
+### tidyverse
 library(dplyr)
 library(lubridate)
 library(purrr)
 library(readr)
 
-## spatial
+### spatial
 library(sf)
 library(tigris)
 
-## other
+### other
 library(janitor)    # data wrangling
 library(rjson)      # parse json
 library(zoo)        # moving averages
@@ -29,22 +53,24 @@ date <- Sys.Date()-1
 
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
-# check ltc metadata for update
-update <- fromJSON(file = "https://data.cms.gov/api/views/metadata/v1/s2uc-8wxp")
-update <- as.Date(update$dataUpdatedAt)
-load("data/source/ltc/last_update.rda")
+# execute daily workflow ####
 
-# ==== # === # === # === # === # === # === # === # === # === # === # === # === #
-
-# execute workflow ####
-## daily workflow elements
 source("source/workflow/01_scrape_and_tidy.R")
 source("source/workflow/02_create_state_msa.R")
 source("source/workflow/03_add_rates.R")
 source("source/workflow/04_create_spatial.R")
 source("source/workflow/05_create_regions.R")
 
-## weekly workflow elements
+# ==== # === # === # === # === # === # === # === # === # === # === # === # === #
+
+# execute weekly workflow ####
+
+## check ltc metadata for update
+update <- fromJSON(file = "https://data.cms.gov/api/views/metadata/v1/s2uc-8wxp")
+update <- as.Date(update$dataUpdatedAt)
+load("data/source/ltc/last_update.rda")
+
+## rebuild ltc data if there has been an update
 if ((update == last_update$current_date) == FALSE){
   source("source/workflow/11_create_ltc.R") 
 }
@@ -52,4 +78,4 @@ if ((update == last_update$current_date) == FALSE){
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
 # clean-up ####
-rm(date, update, last_update)
+rm(date, update, last_update, q, get_last_update)
---
+++ source/functions/get_last_update.R
@@ -0,0 +1,18 @@
+get_last_update <- function(){
+  
+  x <- httr::GET(url = "https://api.github.com/repos/nytimes/covid-19-data/commits/master")
+  y <- x$headers$`last-modified`
+  y <- stringr::word(y, 2, -2)
+  
+  z <- lubridate::dmy_hms(y)
+  z <- as.POSIXlt(z, tz = "EST")
+  current_time <- Sys.time()
+  delta <- as.numeric(current_time-z)
+  
+  q <- usethis::ui_yeah(paste0("The New York Times data were last updated ", round(delta, digits = 2), " hours ago. Proceed?"))
+  
+  return(q)
+  
+}
+
+
