95b833dbef5f8b8886c187a73db1c1046b54d4fd
Christopher Prener <chris.prener@slu.edu>
2020-12-20 13:15:24 -0500

    additional progress on KC zip map work

--- source/archive/create_kc_zips.R
+++ source/archive/create_kc_zips.R
@@ -26,7 +26,7 @@ kc_metro <- st_read("data/county/daily_snapshot_mo_xl.geojson", crs = 4269) %>%
   st_remove_holes()
 
 ## create ZCTA boundaries for KC metro only
-kc_zips <- zctas(year = 2018, class = "sf") %>%
+kc_zips <- zctas(year = 2019, class = "sf") %>%
   st_transform(crs = "+proj=aea +lat_1=29.5 +lat_2=45.5 +lat_0=37.5 +lon_0=-96 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs ") 
 
 kc_zips_clipped <- kc_zips %>%
@@ -34,7 +34,25 @@ kc_zips_clipped <- kc_zips %>%
   select(GEOID10) %>%
   st_collection_extract(type = "POLYGON")
 
+kc_zips_clipped <- kc_zips_clipped %>%
+  mutate(sq_km = measurements::conv_unit(as.numeric(st_area(geometry)), from = "m2", "km2")) %>%
+  select(GEOID10, sq_km) %>%
+  filter(sq_km > .01)
+
 kc_zips <- filter(kc_zips, GEOID10 %in% kc_zips_clipped$GEOID10)
+
+#
+zip_data <- process_kc_zip(dates = dates) %>%
+  filter(report_date == "2020-12-19")
+
+x <- kc_zips_clipped
+st_geometry(x) <- NULL
+x <- full_join(x, zip_data, by = c("GEOID10" = "zip"))
+y <- filter(kc_zips, GEOID10 %in% c("64028", "66160"))
+
+# both of these need to be folded into encompassing zips
+# 66103 <- 66160
+# 64079 <- 64028
 
 # load population data ####
 kc_pop <- get_acs(geography = "zcta", year = 2018, variable = "B01003_001") %>%
@@ -48,5 +66,6 @@ kc_pop <- get_acs(geography = "zcta", year = 2018, variable = "B01003_001") %>%
 # combine geoids and population ####
 kc_zips <- left_join(kc_zips, kc_pop, by = "GEOID10") %>%
   select(GEOID10, total_pop)
+
 
 
