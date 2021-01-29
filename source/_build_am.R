cc82f13dd71ac9c4da8354a0af1543bd6f7c27cd
Christopher Prener <chris.prener@slu.edu>
2021-01-29 14:38:57 -0600

    build am data for 2021-01-29

--- source/_build_am.R
+++ source/_build_am.R
@@ -23,6 +23,9 @@ if (q == FALSE){
   stop("AM update aborted!")
 }
 
+## confirm St. Louis Pandemic Task Force data
+auto_update <- usethis::ui_yeah("Do you want to automatically update the remote GitHub repo?")
+
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
 # dependencies ####
@@ -86,5 +89,16 @@ if ((update == last_update$current_date) == FALSE){
 
 # ==== # === # === # === # === # === # === # === # === # === # === # === # === #
 
+# optionally pushed to GitHub
+if (auto_update == TRUE){
+  
+  system(paste0("git commit -a -m 'build am data for ", as.character(date+1), "'"))
+  system("git add -A")
+  system("git push origin master")
+  
+}
+
+# ==== # === # === # === # === # === # === # === # === # === # === # === # === #
+
 # clean-up ####
-rm(date, update, last_update, q, get_last_update)
+rm(date, auto_update, update, last_update, q, get_last_update)
